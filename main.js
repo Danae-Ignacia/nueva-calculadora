cc5c70a642aea6984d592a9ad6d89fe05bcce1a2
Danae Quevedo <d.ignacia.quevedo.v@gmail.com>
2024-01-29 23:08:57 -0300

    agregando objetos y constantes

--- main.js
+++ main.js
@@ -1,22 +1,33 @@
+alert(`Hola, gracias por utilizar nuestra calculadora!`);
+class Usuario {
+  constructor(info) {
+    this.nombre = info.nombre;
+    this.email = info.email;
+    this.fono = info.fono;
+  }
+  saludo() {
+    alert (`Bienvenido(a) ${this.nombre}, manos a la obra!`);
+  }
+}
+let nombre =  prompt("Ingresa tu nombre").toLowerCase();
+let email = prompt("Ingrese su email");
+let fono = prompt("Ingrese su fono");
+
+const usuarioRegistrado = new Usuario({nombre, email, fono});
+usuarioRegistrado.saludo();
 
 
-let nombre = prompt("Ingresa tu nombre").toLowerCase();
-let cliente = prompt("¿Eres cliente? (Responde si o no)").toLowerCase();
 while (nombre != "") {
-    if (cliente === "si") {
-        alert(`Bienvenido/a ${nombre} gracias por utilizar nuestra calculadora`);
+    const suma = (x, y) => x + y;
+    const oferta = (x, y) => x - y;
+    const iva = (x) => x * 0.19;
+    
+    let precioProducto = Number(prompt("Ingrese el precio del producto"));
+    let descuento = Number(prompt("Ingrese el descuento"));
+    
+    let nuevoPrecio = oferta(suma(precioProducto, iva(precioProducto)), descuento);
 
-        function precio(precioPlan, precioSucursales) {
-            resultado = precioPlan + precioSucursales;
-        }
-        let precioPlan = Number(prompt("Ingrese el valor de su plan"))
-        let numeroSucursales = Number(prompt("Número de sucursales que necesita"))
-        let precioSucursales = numeroSucursales * 23990
-        precio(precioPlan, precioSucursales)
-        alert(`El valor final a pagar sería: ${resultado}`);
-    } else {
-        alert("Hazte cliente");
-    }
+    alert(nuevoPrecio);
 }
 
 
