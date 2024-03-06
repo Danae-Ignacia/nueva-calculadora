2fcee7181bff4a10cca475bfe7747517d56b5963
Danae Quevedo <d.ignacia.quevedo.v@gmail.com>
2024-03-05 23:33:17 -0300

    separar archivos js para dejar uno exclusivo del arreglo de productos y mejoras en el código

--- main.js
+++ main.js
@@ -7,31 +7,6 @@ const cantidadCarrito = document.getElementById("cantidadCarrito");
 
 let carrito = JSON.parse(localStorage.getItem("carrito")) || [];
 
-// Arreglo Productos
-const productos = [
-  {
-    id: 1,
-    nombre: "plan basico",
-    precio: 20032,
-    img: "https://ablog.managemart.com/images/o_1dnpgku1cdiogh18pabq8cb8.jpg",
-    cantidad: 1,
-  },
-  {
-    id: 2,
-    nombre: "plan medio",
-    precio: 20034,
-    img: "https://www.agenciaeplus.com.br/wp-content/uploads/2022/12/canais-de-venda-loja-virtual.jpg",
-    cantidad: 1,
-  },
-  {
-    id: 3,
-    nombre: "plan full",
-    precio: 20036,
-    img: "https://www.bycomercial.com/wp-content/uploads/2019/07/post-508.jpg",
-    cantidad: 1,
-  },
-];
-
 
 // Div de producto para agregar al HTML 
 productos.forEach((product) => {
@@ -39,7 +14,7 @@ productos.forEach((product) => {
   content.className = "box";
   content.innerHTML = `
   <img src="${product.img}" class="content-img">
-  <h2>${product.nombre}</h2>
+  <h2 class="content-name">${product.nombre}</h2>
   <p class="content-price">$ ${product.precio}</p>
   `;
   shopContent.append(content);
